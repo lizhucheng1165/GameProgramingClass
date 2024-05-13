0c3bbf64f8113ec72612b93a67d085ca93a3abe6
lizhucheng1165 <130527082+lizhucheng1165@users.noreply.github.com>
2024-05-13 13:50:27 +0900

    Add files via upload
    수업2

---
+++ LJS2/LJS2/LJS2.cpp
@@ -0,0 +1,19 @@
+﻿// LJS2.cpp : 이 파일에는 'main' 함수가 포함됩니다. 거기서 프로그램 실행이 시작되고 종료됩니다.
+//
+
+#include <iostream>
+
+int main()
+{
+    int nData;
+    int nTest;
+    int nNumber;
+
+    
+    //Ctrl + space를 누르면 사용가능한 명령어나 변수, 함수를 모두 볼 수 있다
+
+
+
+
+}
+
