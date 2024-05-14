aeccaeb6bf122c6de9305c4271f0e883495f30dc
lizhucheng1165 <130527082+lizhucheng1165@users.noreply.github.com>
2024-05-14 13:51:48 +0900

    Add files via upload
    24.05.14

---
+++ LJS3/LJS3/LJS3.cpp
@@ -0,0 +1,24 @@
+﻿// LJS3.cpp : 이 파일에는 'main' 함수가 포함됩니다. 거기서 프로그램 실행이 시작되고 종료됩니다.
+//
+
+#include <iostream>
+
+int main()
+{
+	int nNumber{}; //방정리
+
+
+	nNumber = 10; //대입을 이용한 초기화
+
+
+	std::cout << nNumber;
+
+	//int nNumber1 = 0;//편의상 제공(C언어 사용자 배려)
+	//int nNumber2 (0);//원칙상 초기화의 정석
+	//int nNumber3{ 0 };
+	
+
+
+}
+
+
---
+++ LJS4/LJS4/LJS4.cpp
@@ -0,0 +1,20 @@
+﻿// LJS4.cpp : 이 파일에는 'main' 함수가 포함됩니다. 거기서 프로그램 실행이 시작되고 종료됩니다.
+//
+
+#include <iostream>
+
+int main()
+{
+    int nData1{};
+    int nData2{};
+    int nResult{};
+
+    nData1 = 20;
+    nData2 = 10;
+
+    nResult = (nData1 + nData2) * nData1;
+
+
+    std::cout << nResult;
+}
+
