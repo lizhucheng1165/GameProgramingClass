d5d8a4c6c3af602e7ee1540a5ea319dcb5e468d3
lizhucheng1165 <130527082+lizhucheng1165@users.noreply.github.com>
2024-05-21 13:28:20 +0900

    Add files via upload
    printf, scanf_s
    24.05.21

---
+++ LJS10/LJS10/LJS10.cpp
@@ -0,0 +1,22 @@
+﻿// LJS10.cpp : 이 파일에는 'main' 함수가 포함됩니다. 거기서 프로그램 실행이 시작되고 종료됩니다.
+//
+
+#include <iostream>
+
+int main()
+{
+	int nData{};
+
+	//scanf_s("입력: %d", &nData);
+	//scanf_s("%d", nData);
+	//scanf_s("%d\n", &nData);
+	scanf_s("%d", &nData);
+
+
+	printf("출력: %d\n", nData);
+
+}
+
+
+
+
