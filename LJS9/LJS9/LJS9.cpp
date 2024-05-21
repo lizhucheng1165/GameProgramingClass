3494c3cb092e88bbcca4a8f64520dbe6fb9b474a
lizhucheng1165 <130527082+lizhucheng1165@users.noreply.github.com>
2024-05-21 11:58:00 +0900

    Add files via upload
    24.05.20

---
+++ LJS9/LJS9/LJS9.cpp
@@ -0,0 +1,36 @@
+﻿// LJS9.cpp : 이 파일에는 'main' 함수가 포함됩니다. 거기서 프로그램 실행이 시작되고 종료됩니다.
+//
+
+#include <iostream>
+
+int main()
+{
+
+	//int nData{};
+
+	//nData = 1 + 2 + 3;
+
+	///*if (nData > 5)
+	//{
+	//	printf("5보다 크다\n");
+	//}
+
+	//if (nData > 5) printf("5보다 크다\n");*/
+
+
+	///*if (0)
+	//{
+	//	printf("테스트\n");
+	//}*/
+
+	int nData1{};
+	int nData2{};
+
+	nData1 = nData2 = 10;
+
+
+
+
+
+}
+
