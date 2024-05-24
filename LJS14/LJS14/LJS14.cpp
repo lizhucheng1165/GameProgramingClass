82f7c88364d3995d7cbe2b9df847ade8da01bb81
lizhucheng1165 <130527082+lizhucheng1165@users.noreply.github.com>
2024-05-24 11:58:56 +0900

    Add files via upload
    240524

---
+++ LJS14/LJS14/LJS14.cpp
@@ -0,0 +1,22 @@
+﻿// LJS14.cpp : 이 파일에는 'main' 함수가 포함됩니다. 거기서 프로그램 실행이 시작되고 종료됩니다.
+//
+
+#include <iostream>
+
+int main()
+{
+	int nData{};
+
+	scanf_s("%d", &nData);
+
+	/*if (nData > 5 && nData < 10)
+		printf("5보다 크고 10보다 작다\n");*/
+
+
+	if (nData < 5 || nData >10)
+		printf("5보다 작거나 10보다 크다\n");
+
+
+}
+
+
---
+++ LJS15/LJS15/LJS15.cpp
@@ -0,0 +1,29 @@
+﻿// LJS15.cpp : 이 파일에는 'main' 함수가 포함됩니다. 거기서 프로그램 실행이 시작되고 종료됩니다.
+//
+
+#include <iostream>
+
+int main()
+{
+	int nNumber{};
+	int nRemainder{};
+
+	scanf_s("%d", &nNumber);
+
+	nRemainder = nNumber % 2;
+
+	if (nNumber == 0)
+	{
+		printf("판단불가\n");
+	}
+
+	if (nNumber != 0 && nRemainder == 0)
+	{
+		printf("짝수\n");
+	}
+
+	if (nRemainder == 1)
+	{
+		printf("홀수\n");
+	}
+}
