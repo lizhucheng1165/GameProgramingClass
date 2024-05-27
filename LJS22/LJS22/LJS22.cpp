ddc272fa0dad6f5f0f964083f196807f7b198fa2
lizhucheng1165 <130527082+lizhucheng1165@users.noreply.github.com>
2024-05-27 13:28:33 +0900

    Add files via upload
    24.05.27
    while문

---
+++ LJS22/LJS22/LJS22.cpp
@@ -0,0 +1,27 @@
+﻿// LJS22.cpp : 이 파일에는 'main' 함수가 포함됩니다. 거기서 프로그램 실행이 시작되고 종료됩니다.
+//
+
+#include <iostream>
+
+int main()
+{
+	int nData{};
+	int nTotal{};
+	nData = 1;
+
+	while (nData <= 10)
+	{
+		printf("%d\n", nData);
+
+		nData = nData + 1;
+	}
+
+	while (nData <= 10)
+	{
+		nTotal += nData;
+
+		nData++;
+	}
+
+	printf("%d", nTotal);
+}
