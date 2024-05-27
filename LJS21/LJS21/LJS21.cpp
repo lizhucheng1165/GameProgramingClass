bc5adbfba2e0379aac812180c6394f80589fe34f
lizhucheng1165 <130527082+lizhucheng1165@users.noreply.github.com>
2024-05-27 12:55:13 +0900

    Add files via upload
    24.05.27
    전증가 후증가
    while문

---
+++ LJS21/LJS21/LJS21.cpp
@@ -0,0 +1,38 @@
+﻿// LJS21.cpp : 이 파일에는 'main' 함수가 포함됩니다. 거기서 프로그램 실행이 시작되고 종료됩니다.
+//
+
+#include <iostream>
+
+int main()
+{
+	int nData{};
+
+	nData = 1;
+
+	/*while (nData < 5)
+	{
+		printf("참");
+		nData = 10;
+	}*/
+	/*
+	nData = nData + 1;
+
+
+	nData += 1;
+
+
+	nData = 1 + nData;
+
+	++nData;
+	nData++;*/
+
+
+	//printf("%d", nData++);
+	//printf("%d", ++nData);
+	printf("%d", nData);
+
+	/*printf("%d, %d, %d", nData++, nData++, nData++);
+	printf("%d, %d, %d", ++nData,++nData, ++nData);*/
+}
+
+
