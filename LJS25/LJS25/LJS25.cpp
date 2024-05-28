d4f7634e22ba5b135d0f5e2e9e7c3acfd1d3b999
lizhucheng1165 <130527082+lizhucheng1165@users.noreply.github.com>
2024-05-28 13:48:07 +0900

    Add files via upload
    24.05.28
    약수구하기

---
+++ LJS25/LJS25/LJS25.cpp
@@ -0,0 +1,29 @@
+﻿// LJS25.cpp : 이 파일에는 'main' 함수가 포함됩니다. 거기서 프로그램 실행이 시작되고 종료됩니다.
+//
+
+#include <iostream>
+
+int main()
+{
+	int nData{};
+	int nDivideNumber{};
+	int nRemainder{};
+
+	scanf_s("%d", &nData);
+
+	nDivideNumber = nData;
+	
+	while (nDivideNumber > 0)
+	{
+		nRemainder = nData % nDivideNumber;
+
+		if (nRemainder == 0)
+		{
+			printf("%d\n", nDivideNumber);
+		}
+
+		nDivideNumber--;
+	}
+
+}
+
