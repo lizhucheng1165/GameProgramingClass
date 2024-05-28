4246cc2237681b46e49e53906c89651c956d33ec
lizhucheng1165 <130527082+lizhucheng1165@users.noreply.github.com>
2024-05-28 11:58:16 +0900

    Add files via upload
    2024.05.28
    작은수에서 큰수까지 합

---
+++ LJS23/LJS23/LJS23.cpp
@@ -0,0 +1,46 @@
+﻿// LJS23.cpp : 이 파일에는 'main' 함수가 포함됩니다. 거기서 프로그램 실행이 시작되고 종료됩니다.
+//
+
+#include <iostream>
+
+int main()
+{
+	//int형 두 수를 입력받아
+	//작은 수부터 큰 수까지의 총합을 구한다
+
+	//ex) 5 1 => 1~5까지 합   7 2 -> 2~7까지의 합
+
+	int nNumber1{};
+	int nNumber2{};
+	int nSmallNumber{};
+	int nLargeNumber{};
+	int nLoopCount{};
+	int nTotal{};
+
+	scanf_s("%d" , &nNumber1);
+	scanf_s("%d" , &nNumber2);
+
+	nSmallNumber = nNumber1;
+	nLargeNumber = nNumber2;
+
+	if (nSmallNumber > nNumber2)
+	{
+		nSmallNumber = nNumber2;
+		nLargeNumber = nNumber1;
+	}
+
+	nLoopCount = nLargeNumber - nSmallNumber;
+	nTotal = nSmallNumber;
+
+	while (nLoopCount > 0)
+	{
+		nTotal += 1;
+
+
+		nLoopCount--;
+	}
+
+	printf("%d\n", nTotal);
+
+}
+
