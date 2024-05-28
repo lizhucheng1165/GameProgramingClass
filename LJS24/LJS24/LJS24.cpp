eef0fd5470f552dbe360f9f7bb965b166684be79
lizhucheng1165 <130527082+lizhucheng1165@users.noreply.github.com>
2024-05-28 12:50:13 +0900

    Add files via upload
    24.05.28

---
+++ LJS24/LJS24/LJS24.cpp
@@ -0,0 +1,35 @@
+﻿// LJS24.cpp : 이 파일에는 'main' 함수가 포함됩니다. 거기서 프로그램 실행이 시작되고 종료됩니다.
+//
+
+#include <iostream>
+
+int main()
+{
+	int nNumber1{};
+	int nNumber2{};
+	int nTotal{};
+	int nSmallNumber{};
+	int nLargeNumber{};
+	int nData{};
+
+	scanf_s("%d", &nNumber1);
+	scanf_s("%d", &nNumber2);
+
+	nSmallNumber = nNumber1;
+	nLargeNumber = nNumber2;
+
+	if (nNumber1 >= nNumber2)
+	{
+		nSmallNumber = nNumber2;
+		nLargeNumber = nNumber1;
+	}
+
+	nData = nSmallNumber;
+	while (nData <= nLargeNumber)
+	{
+		nTotal += nData;
+		nData++;
+	}
+
+	printf("%d", nTotal);
+}
