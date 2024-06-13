0b4ff0ccc6c6f5a12e824b0efb27b176dc32a95d
lizhucheng1165 <130527082+lizhucheng1165@users.noreply.github.com>
2024-06-13 13:28:42 +0900

    Add files via upload
    2차원 배열예

---
+++ LJS2-02/LJS2-02/LJS2-02.cpp
@@ -0,0 +1,22 @@
+﻿// LJS2-02.cpp : 이 파일에는 'main' 함수가 포함됩니다. 거기서 프로그램 실행이 시작되고 종료됩니다.
+//
+
+#include <iostream>
+
+int main()
+{
+	int arData[2][3]{ {6,5,4}, {3,2,1} };
+	
+
+	for (int i = 0; i < 2; i++)
+	{
+		for (int j = 0; j < 3; j++)
+		{
+			printf("%d", arData[i][j]);
+		}
+		printf("\n");
+	}
+
+
+
+}
