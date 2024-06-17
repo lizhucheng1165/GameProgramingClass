680fee8f844b2194e0db27dacfed38c1703107ad
lizhucheng1165 <130527082+lizhucheng1165@users.noreply.github.com>
2024-06-17 13:51:36 +0900

    Add files via upload
    2차원 배열 예제
    마방진

---
+++ LJS2-07/LJS2-07/LJS2-07.cpp
@@ -0,0 +1,53 @@
+﻿// LJS2-07.cpp : 이 파일에는 'main' 함수가 포함됩니다. 거기서 프로그램 실행이 시작되고 종료됩니다.
+//
+
+#include <iostream>
+
+int main()
+{
+	int arData[5][5]{};
+	int nFirstIndex{};
+	int nSecondIndex{};
+	int nData{};
+
+	nFirstIndex = 0;
+	nSecondIndex = 2;
+	nData = 1;
+
+	arData[nFirstIndex][nSecondIndex] = nData;
+	nData++;
+
+	nFirstIndex += 4;
+	nSecondIndex++;
+
+	arData[nFirstIndex][nSecondIndex] = nData;
+	nData++;
+
+	nFirstIndex -= 1;
+	nSecondIndex++;
+
+	arData[nFirstIndex][nSecondIndex] = nData;
+	nData++;
+
+	nFirstIndex -= 1;
+	nSecondIndex -= 4;
+
+	arData[nFirstIndex][nSecondIndex] = nData;
+	nData++;
+
+	nFirstIndex -= 1;
+	nSecondIndex++;
+
+	arData[nFirstIndex][nSecondIndex] = nData;
+	nData++;
+
+	for (int i = 0; i < 5; i++)
+	{
+		for (int j = 0; j < 5; j++)
+		{
+			printf("%2d ", arData[i][j]);
+		}
+		printf("\n");
+	}
+
+}
