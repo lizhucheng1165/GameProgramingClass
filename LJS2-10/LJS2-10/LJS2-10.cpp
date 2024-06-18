6e4a5875bbd7eb58d7216dcb20598fd324f8c47f
lizhucheng1165 <130527082+lizhucheng1165@users.noreply.github.com>
2024-06-18 13:49:05 +0900

    Add files via upload
    달팽이배열

---
+++ LJS2-10/LJS2-10/LJS2-10.cpp
@@ -0,0 +1,23 @@
+﻿// LJS2-10.cpp : 이 파일에는 'main' 함수가 포함됩니다. 거기서 프로그램 실행이 시작되고 종료됩니다.
+//
+
+#include <iostream>
+
+int main()
+{
+	int nCount{};
+	int nData{};
+
+	nData = 10;
+	for (int i = 0; i < 9; i++)
+	{
+		nCount = nData / 2;
+		for (int j = 0; j < nCount; j++)
+		{
+			//index1 ,2 를 계산 / 스위치처럼 방향이 반대로 전환되는 기능 추가
+
+		}
+		nData--;
+	}
+}
+
