b2482009c44ee6309d29c5057173111de83c2243
lizhucheng1165 <130527082+lizhucheng1165@users.noreply.github.com>
2024-06-20 13:28:52 +0900

    Add files via upload
    포인터

---
+++ LJS2-12/LJS2-12/LJS2-12.cpp
@@ -0,0 +1,18 @@
+﻿// LJS2-12.cpp : 이 파일에는 'main' 함수가 포함됩니다. 거기서 프로그램 실행이 시작되고 종료됩니다.
+//
+
+#include <iostream>
+
+int main()
+{
+	int nData{};
+	int* p{};
+	
+	nData = 100;
+	p = &nData; //인트 경로를 p에 저장
+
+	printf("%d\n", *p);
+	*p = 99;
+	printf("%d\n", nData);
+
+}
