0d6e841681e74d1cd16b3b72b58a048781525252
lizhucheng1165 <130527082+lizhucheng1165@users.noreply.github.com>
2024-06-21 13:51:20 +0900

    Add files via upload
    포인터 배열

---
+++ LJS2-16/LJS2-16/LJS2-16.cpp
@@ -0,0 +1,23 @@
+﻿// LJS2-16.cpp : 이 파일에는 'main' 함수가 포함됩니다. 거기서 프로그램 실행이 시작되고 종료됩니다.
+//
+
+#include <iostream>
+
+int main()
+{
+	int arData[5]{ 5,4,3,2,1 };
+	//int* p[5]{ nullptr,nullptr,nullptr,nullptr,nullptr };
+	int(*pAr)[5] {};
+	int* p{};
+
+	pAr = &arData;
+
+	p = (*pAr);
+
+	printf("%d\n", (*pAr)[2]);
+	printf("%d\n", *((*pAr) + 2));
+
+
+
+
+}
