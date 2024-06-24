87d6234247d6c8f3ac0b5b83ec06f728201f4303
lizhucheng1165 <130527082+lizhucheng1165@users.noreply.github.com>
2024-06-24 13:14:49 +0900

    Add files via upload
    배열포인터

---
+++ LJS2-17/LJS2-17/LJS2-17.cpp
@@ -0,0 +1,28 @@
+﻿// LJS2-17.cpp : 이 파일에는 'main' 함수가 포함됩니다. 거기서 프로그램 실행이 시작되고 종료됩니다.
+//
+
+#include <iostream>
+
+int main()
+{
+	int arData[2][3]{ {6,5,4},{3,2,1} };
+	int(*p)[3] {};
+
+	p = arData;
+	p = &arData[0];
+
+	for (int i = 0; i < 3; i++)
+	{
+		//printf("%d\n", (*p)[i]);
+		printf("%d\n", (*(p + 1))[i]);
+	}
+	printf("\n");
+
+	for (int i = 0; i < 3; i++)
+	{
+		//printf("%d\n", (p[1])[i]);
+		printf("%d\n", p[1][i]);
+	}
+	printf("\n");
+
+}
