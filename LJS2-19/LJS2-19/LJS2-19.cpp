a6f4118595be7d731c42d40baa4b52c92887bee1
lizhucheng1165 <130527082+lizhucheng1165@users.noreply.github.com>
2024-06-25 13:52:20 +0900

    Add files via upload
    함

---
+++ LJS2-19/LJS2-19/LJS2-19.cpp
@@ -0,0 +1,19 @@
+﻿// LJS2-19.cpp : 이 파일에는 'main' 함수가 포함됩니다. 거기서 프로그램 실행이 시작되고 종료됩니다.
+//
+
+#include <iostream>
+
+void function(int nData);
+
+int main()
+{
+	int nMain{};
+	nMain = 10;
+
+	function(nMain);
+}
+
+void function(int nData)
+{
+	printf("%d\n", nData);
+}
---
+++ LJS2-20/LJS2-20/LJS2-20.cpp
@@ -0,0 +1,27 @@
+﻿// LJS2-20.cpp : 이 파일에는 'main' 함수가 포함됩니다. 거기서 프로그램 실행이 시작되고 종료됩니다.
+//
+
+#include <iostream>
+
+void funcValue(int nData);
+void funcPointer(int* pData);
+
+
+int main()
+{
+	int nMain{};
+	nMain = 100;
+
+	funcValue(nMain);
+	funcPointer(&nMain);
+}
+
+void funcValue(int nData)
+{
+	printf("value : %d\n", nData);
+}
+
+void funcPointer(int* pData)
+{
+	printf("pointer : %d\n", *pData);
+}
