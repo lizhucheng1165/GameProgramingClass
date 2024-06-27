7c9ca97bee99934177d0414b5b8ae9ec966090a6
lizhucheng1165 <130527082+lizhucheng1165@users.noreply.github.com>
2024-06-27 13:53:30 +0900

    Add files via upload
    cosnt문법

---
+++ LJS2-22/LJS2-22/LJS2-22.cpp
@@ -0,0 +1,38 @@
+﻿// LJS2-22.cpp : 이 파일에는 'main' 함수가 포함됩니다. 거기서 프로그램 실행이 시작되고 종료됩니다.
+//
+
+#include <iostream>
+
+void printData(const int* pData, int nLength);
+
+int main()
+{
+	//const int nData{};
+	//int const nData{};
+
+	//nData = 100;
+	//const int* p{};
+	//int const *p{};
+
+	//*p = 10;
+
+	//const int* const** const* p{};
+	//int const* const** const* p{};
+
+	//****p = 0;
+
+	int arData[5]{ 5,4,3,2,1 };
+
+	printData(arData, 5);
+
+	
+
+}
+
+void printData(const int* pData, int nLength)
+{
+	for (int i = 0; i < nLength; i++)
+	{
+		printf("%d\n", pData[i]);
+	}
+}
