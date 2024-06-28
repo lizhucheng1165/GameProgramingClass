3b6eab8265abc17ea3b04633fa9ad6ac1ede4fe8
lizhucheng1165 <130527082+lizhucheng1165@users.noreply.github.com>
2024-06-28 13:44:55 +0900

    Add files via upload
    함수

---
+++ LJS2-23/LJS2-23/LJS2-23.cpp
@@ -0,0 +1,33 @@
+﻿// LJS2-23.cpp : 이 파일에는 'main' 함수가 포함됩니다. 거기서 프로그램 실행이 시작되고 종료됩니다.
+//
+
+#include <iostream>
+
+//int 형 두 수의 합을 구하는 함수
+void sumTwoIntiger(int nNumber1, int nNumber2, int* pResult);
+
+//세개의 숫자의 최대공약수를 구하는 함수
+void getHighestCommonDivisor(int nNumber1, int nNumber2,int nNumber3, int* pResult);
+
+//삼각형 세변의 길이로 넓이를 구하는 함수
+void getTriangleAreaWithThreeSides(float fSide1, float fSide2, float fSide3, float* pTriangleArea);
+
+//배열에서 가장 작은숫자의 반복 횟수를구하는 함수
+void getMinimumNumbersRepeatCountInArray(const int* pData, int nDataLength, int* pRepeatCount);
+
+//배열을 배열에 복사하는 함수
+void copyArrayToAnotherArray(const int* pData, int nDataLength, int* pCopy, int pCopyLength );
+
+//특정 숫자를 배열에 일정 횟수만큼 복사해 주는 함수
+void fillNumbersInArray(int nCopyNumber, int nCopyCount, int* pResultArray, int* nResultArrayLength);
+
+//두 개의 숫자를 바꿔주는 함수  
+void swapTwoNumber(int* nNumber1, int* nNumber2);
+
+
+int main()
+{
+
+
+}
+
