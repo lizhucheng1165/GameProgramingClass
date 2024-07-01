62333a08bf86c7ac257940db1c237bedde435101
lizhucheng1165 <130527082+lizhucheng1165@users.noreply.github.com>
2024-07-01 13:28:59 +0900

    Add files via upload
    return

---
+++ LJS2-25/LJS2-25/LJS2-25.cpp
@@ -0,0 +1,36 @@
+﻿// LJS2-25.cpp : 이 파일에는 'main' 함수가 포함됩니다. 거기서 프로그램 실행이 시작되고 종료됩니다.
+//
+
+#include <iostream>
+
+int add(int nData1, int nData2);
+
+int main()
+{
+	int nResult{};
+
+	//nResult = add(10, 20);
+
+	//printf("%d\n", nResult);
+	//printf("%d\n", add(10,20));
+
+
+	//for (int i = 0; i < 10; i++)
+	//{
+	//	printf("%d\n", add(10, 20));
+
+	//}
+
+	nResult = add(10, 20);
+	for (int i = 0; i < 10; i++)
+	{
+		printf("%d\n",nResult);
+
+	}
+}
+
+int add(int nData1, int nData2)
+{
+	return nData1 + nData2;
+
+}
