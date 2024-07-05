fda80dfe8b35ffb4286638b65c68fa6be0eb865a
lizhucheng1165 <130527082+lizhucheng1165@users.noreply.github.com>
2024-07-05 13:09:52 +0900

    Add files via upload
    return

---
+++ LJS2-32/LJS2-32/LJS2-32.cpp
@@ -0,0 +1,31 @@
+﻿// LJS2-32.cpp : 이 파일에는 'main' 함수가 포함됩니다. 거기서 프로그램 실행이 시작되고 종료됩니다.
+//
+
+#include <iostream>
+void printData(const int* pdata, int nLength);
+
+int main()
+{
+	int arData[10]{};
+	int nDataLength{};
+	int nUseLength{};
+
+	nDataLength = 10;
+	while (nUseLength < nDataLength)
+	{
+		scanf_s("%d", &arData[nUseLength]);
+		nUseLength++;
+	}
+
+	printData(arData, 10);
+}
+
+void printData(const int* pdata, int nLength)
+{
+	for (int i = 0; i < nLength; i++)
+	{
+		printf("%d ", pdata[i]);
+	}
+	printf("\n");
+
+}
