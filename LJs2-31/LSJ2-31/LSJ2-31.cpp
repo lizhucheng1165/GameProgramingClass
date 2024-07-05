63c4f9496ad4e6728d062b4ff07e3c3ac7711fa0
lizhucheng1165 <130527082+lizhucheng1165@users.noreply.github.com>
2024-07-05 13:11:10 +0900

    Add files via upload
    return

---
+++ LJs2-31/LSJ2-31/LSJ2-31.cpp
@@ -0,0 +1,21 @@
+﻿// LSJ2-31.cpp : 이 파일에는 'main' 함수가 포함됩니다. 거기서 프로그램 실행이 시작되고 종료됩니다.
+//
+
+#include <iostream>
+
+
+int add(int n1, int n2, int n3, int n4 = 0);
+
+int main()
+{
+	printf("%d\n", add(1, 2, 3, 4));
+	printf("%d\n", add(1, 2, 3));
+
+	
+}
+
+int add(int n1, int n2, int n3, int n4)
+{
+	return n1 + n2 + n3 + n4;
+
+}
