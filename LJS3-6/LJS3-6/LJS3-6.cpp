aee050117b5368478f763ba58a3635028e9b0103
lizhucheng1165 <130527082+lizhucheng1165@users.noreply.github.com>
2024-07-12 13:53:33 +0900

    Add files via upload
    구조

---
+++ LJS3-6/LJS3-6/LJS3-6.cpp
@@ -0,0 +1,37 @@
+﻿// LJS3-6.cpp : 이 파일에는 'main' 함수가 포함됩니다. 거기서 프로그램 실행이 시작되고 종료됩니다.
+//
+
+#include <iostream>
+struct S_DATA
+{
+	//(1,1, 빈공간 2),4,4 -> 12
+	//char c1;
+	//char c2;
+	//int n1;
+	//int n2;
+
+	//4,4,4,4바이트 -> 16
+	//char c1;
+	//int n1;
+	//char c2;
+	//int n2;
+
+	char c1;
+	char c2;
+	int n1;
+	int n2;
+};
+
+
+int main()
+{
+	//S_DATA sData = { 'a','b', 10, 20 };
+	S_DATA sData;
+
+	memset(&sData, 0, sizeof(S_DATA));
+
+	printf("%d\n", sData.n1);
+
+	//printf("%zd\n", sizeof(S_DATA));
+	
+}
