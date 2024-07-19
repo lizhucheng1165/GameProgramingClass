4c774f37295aabba4ea63f2524782fe9f262be36
lizhucheng1165 <130527082+lizhucheng1165@users.noreply.github.com>
2024-07-19 13:52:25 +0900

    Add files via upload
    복사생성자

---
+++ LJS3-13/LJS3-13/LJS3-13.cpp
@@ -0,0 +1,22 @@
+﻿// LJS3-13.cpp : 이 파일에는 'main' 함수가 포함됩니다. 거기서 프로그램 실행이 시작되고 종료됩니다.
+//
+
+#include <iostream>
+
+class C_DATA
+{};
+
+void func(C_DATA c);
+
+int main()
+{
+	C_DATA c1{};
+
+	func(c1);
+
+}
+
+void func(C_DATA c)
+{
+
+}
