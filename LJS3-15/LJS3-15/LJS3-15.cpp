819efcdb356b10408d5fdaf7e9782615c133b1af
lizhucheng1165 <130527082+lizhucheng1165@users.noreply.github.com>
2024-07-22 13:46:15 +0900

    Add files via upload
    reference

---
+++ LJS3-15/LJS3-15/LJS3-15.cpp
@@ -0,0 +1,37 @@
+﻿// LJS3-15.cpp : 이 파일에는 'main' 함수가 포함됩니다. 거기서 프로그램 실행이 시작되고 종료됩니다.
+//
+
+#include <iostream>
+//void pirnt(const int &arData[5]);
+//void print(const int (&arData)[5]);
+//void func(int nData);
+//void func(const int& nData);
+
+
+int main()
+{
+	//int arMain[5]{ 5,4,3,2,1 };
+	////int arMain2[3]{ 5,4,3 };
+
+	//print(arMain);
+	////print(arMain2);
+
+	//func(100);
+}
+
+//void print(const int(&arData)[5])
+//{
+//	for (int i = 0; i < 5; i++)
+//	{
+//		printf("%d ", arData[i]);
+//	}
+//	printf("\n");
+//}
+
+void func(int nData)
+{
+}
+
+void func(const int& nData)
+{
+}
