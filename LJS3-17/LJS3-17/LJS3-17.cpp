aa310ecb1805bbeee0c4dd1464dfdfc570087dca
lizhucheng1165 <130527082+lizhucheng1165@users.noreply.github.com>
2024-07-24 12:55:09 +0900

    Add files via upload
    메모리 누출

---
+++ LJS3-17/LJS3-17/LJS3-17.cpp
@@ -0,0 +1,38 @@
+﻿// LJS3-17.cpp : 이 파일에는 'main' 함수가 포함됩니다. 거기서 프로그램 실행이 시작되고 종료됩니다.
+//
+
+#include <iostream>
+
+int main()
+{
+	//int* p1{};
+	//int* p2{};
+	//int* p3{};
+
+	//p1 = new int{};
+	//p2 = p1;
+	//p3 = p1;
+
+	//*p3 = 100;
+	//printf("%d\n", *p1);
+
+	//delete p1;
+	//p1 = nullptr;
+
+	int* p{};
+	int nLength{};
+
+	scanf_s("%d", &nLength);
+
+	p = new int[nLength]{};
+
+	for (int i = 0; i < nLength; i++)
+	{
+		printf("%d ", p[i]);
+	}
+	printf("\n");
+
+	//delete p;
+	delete []p;
+	p = nullptr;
+}
---
+++ LJS3-18/LJS3-18/LJS3-18.cpp
@@ -0,0 +1,22 @@
+﻿// LJS3-18.cpp : 이 파일에는 'main' 함수가 포함됩니다. 거기서 프로그램 실행이 시작되고 종료됩니다.
+//
+#define _CRTDBG_MAP_ALLOC
+#include <stdlib.h>
+#include <crtdbg.h>
+
+#include <iostream>
+
+int main()
+{
+	_CrtSetDbgFlag(_CRTDBG_ALLOC_MEM_DF | _CRTDBG_LEAK_CHECK_DF);
+
+	int* p1{};
+	int* p2{};
+	int* p3{};
+
+	p1 = new int{};
+	p2 = new int{};
+	p3 = new int[5]{};
+
+	*p2 = 9;
+}
