d7f23251c0a946d296c7413ca4237fb4dbf50a32
lizhucheng1165 <130527082+lizhucheng1165@users.noreply.github.com>
2024-08-07 13:24:05 +0900

    Add files via upload
    Enum

---
+++ LJS04-06/LJS04-06/LJS04-06.cpp
@@ -0,0 +1,23 @@
+﻿// LJS04-06.cpp : 이 파일에는 'main' 함수가 포함됩니다. 거기서 프로그램 실행이 시작되고 종료됩니다.
+//
+
+#include <iostream>
+
+enum E_TEST
+{
+	E_TEST_A,
+	E_TEST_B,
+	E_TEST_C,
+};
+
+int main()
+{
+	//printf("%d\n", E_TEST_A);
+	//printf("%d\n", E_TEST_B);
+	//printf("%d\n", E_TEST_C);
+	E_TEST eTest{};
+
+	//eTest = 4;
+	//eTest = E_TEST::
+}
+
---
+++ LJS04-07/LJS04-07/LJS04-07.cpp
@@ -0,0 +1,22 @@
+﻿// LJS04-07.cpp : 이 파일에는 'main' 함수가 포함됩니다. 거기서 프로그램 실행이 시작되고 종료됩니다.
+//
+
+#include <iostream>
+
+enum class E_TEST
+{
+	E_NONE = 0,
+	E_A = 10,
+	E_B,
+	E_C,
+};
+
+int main()
+{
+	E_TEST eTest{};
+
+	//eTest = E_TEST::E_A;
+
+	printf("%d\n", eTest);
+}
+
