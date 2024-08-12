1b42286277f5e6498dcc0e425c6759e8f47ad14f
lizhucheng1165 <130527082+lizhucheng1165@users.noreply.github.com>
2024-08-12 13:46:07 +0900

    Add files via upload
    자료구조, 삽입정렬

---
+++ LJS04-12/LJS04-12/LJS04-12.cpp
@@ -0,0 +1,56 @@
+﻿#include <iostream>
+
+void InsertionLogic(int* arData, int nLength);
+int FindInsertionIndex(const int* arData, int nLength, int nKeyValue);
+void MoveElementBackwards(int* arData, int nLength, int nInsertionIndex);
+void Print(const int* arData, int nLength);
+
+int main()
+{
+    int arData[7]{ 1, 2, 4, 5, 6, 7, 3 };
+
+    int* pData = arData;
+    int nLength = 7;
+
+    InsertionLogic(pData, nLength);
+    Print(pData, nLength);
+}
+
+void InsertionLogic(int* arData, int nLength)
+{
+    int nLastIndex = nLength - 1;
+    int nKeyValue = arData[nLength - 1];
+    int nInsertionIndex = FindInsertionIndex(arData, nLength, nKeyValue);
+
+    MoveElementBackwards(arData, nLength, nInsertionIndex);
+    arData[nInsertionIndex] = nKeyValue;
+}
+
+int FindInsertionIndex(const int* arData, int nLength, int nKeyValue)
+{
+    for (int i = nLength - 2; i >= 0; i--)
+    {
+        if (arData[i] <= nKeyValue)
+        {
+            return i + 1;
+        }
+    }
+    return 0;
+}
+
+void MoveElementBackwards(int* arData, int nLength, int nInsertionIndex)
+{
+    for (int i = nLength - 2; i >= nInsertionIndex; i--)
+    {
+        arData[i + 1] = arData[i];
+    }
+}
+
+void Print(const int* arData, int nLength)
+{
+    for (int i = 0; i < nLength; i++)
+    {
+        printf("%d ", arData[i]);
+    }
+    printf("\n");
+}
---
+++ LJS04-13/LJS04-13/LJS04-13.cpp
@@ -0,0 +1,48 @@
+﻿// LJS04-13.cpp : 이 파일에는 'main' 함수가 포함됩니다. 거기서 프로그램 실행이 시작되고 종료됩니다.
+//
+
+#include <iostream>
+
+void InsertionSortLogic(int* arData, int nLength);
+void InsertionSort(int* arData, int nLength);
+void PrintData(const int* arData, int nLength);
+int main()
+{
+	int arData[7]{ 1,2,4,5,6,7,3 };
+
+	int* pData = arData;
+	int nLength = 7;
+	
+	InsertionSort(pData, nLength);
+	PrintData(pData, nLength);
+}
+
+void InsertionSortLogic(int* arData, int nLength)
+{
+	int nKeyVaue = arData[nLength - 1];
+	int nIndex = nLength - 2;
+	while (nIndex >= 0 && nKeyVaue < arData[nIndex])
+	{
+		arData[nIndex + 1] = arData[nIndex];
+		nIndex--;
+	}
+	arData[nIndex + 1] = nKeyVaue;
+}
+
+void InsertionSort(int* arData, int nLength)
+{
+	for (int i = 2; i <= nLength; i++)
+	{
+		InsertionSortLogic(arData, i);
+	}
+}
+
+void PrintData(const int* arData, int nLength)
+{
+	for (int i = 0; i < nLength; i++)
+	{
+		printf("%d ", arData[i]);
+	}
+	printf("\n");
+}
+
