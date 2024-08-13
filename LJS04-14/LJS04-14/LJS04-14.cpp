49ed7388adcc15269de0ddddbefb6fe81711db39
lizhucheng1165 <130527082+lizhucheng1165@users.noreply.github.com>
2024-08-13 13:51:17 +0900

    Add files via upload
    자료구조 버블정렬

---
+++ LJS04-14/LJS04-14/LJS04-14.cpp
@@ -0,0 +1,60 @@
+﻿// LJS04-14.cpp : 이 파일에는 'main' 함수가 포함됩니다. 거기서 프로그램 실행이 시작되고 종료됩니다.
+//
+
+#include <iostream>
+
+void SwapData(int& n1, int& n2);
+void BubbleSortLogic(int* arData, int nLength);
+void BubbleSort(int* arData, int nLength);
+
+void Print(const int* arData, int nLength);
+
+int main()
+{
+	int arData[5]{ 7,4,5,1,3 };
+
+	BubbleSort(arData, 5);
+
+	Print(arData, 5);
+}
+
+void SwapData(int& n1, int& n2)
+{
+	int nTmp = n1;
+	n1 = n2;
+	n2 = nTmp;
+}
+
+void BubbleSortLogic(int* arData, int nLength)
+{
+	int nCurrentIndex{};
+	int nLastIndex = nLength - 1;
+
+	while (nCurrentIndex < nLastIndex)
+	{
+		int nNextIndex = nCurrentIndex + 1;
+		if (arData[nCurrentIndex] > arData[nNextIndex])
+		{
+			SwapData(arData[nCurrentIndex], arData[nNextIndex]);
+		}
+		nCurrentIndex++;
+	}
+}
+
+void BubbleSort(int* arData, int nLength)
+{
+	while (nLength > 1 )
+	{
+		BubbleSortLogic(arData, nLength);
+		nLength--;
+	}
+}
+
+void Print(const int* arData, int nLength)
+{
+	for (int i = 0; i < nLength; i++)
+	{
+		printf("%d ", arData[i]);
+	}
+	printf("\n");
+}
---
+++ LJS04-15/LJS04-15/LJS04-15.cpp
@@ -0,0 +1,59 @@
+﻿// LJS04-15.cpp : 이 파일에는 'main' 함수가 포함됩니다. 거기서 프로그램 실행이 시작되고 종료됩니다.
+//
+
+#include <iostream>
+
+void SwapData(int& nDst, int& nSrc);
+void BubbleLogic(int& nFirst, int& nSecond);
+void BubbleOneCycle(int* pData, int nLength);
+void BubbleSort(int* pData, int nLength);
+
+void PrintData(const int* pData, int nLength);
+int main()
+{
+    int arData[9]{ 6,4,5,8,7,9,1,2,3 };
+
+    BubbleSort(arData, 9);
+    PrintData(arData, 9);
+}
+
+void SwapData(int& nDst, int& nSrc)
+{
+    int nTmp = nDst;
+    nDst = nSrc;
+    nSrc = nTmp;
+}
+
+void BubbleLogic(int& nFirst, int& nSecond)
+{
+    if (nSecond < nFirst)
+    {
+        SwapData(nFirst, nSecond);
+    }
+}
+
+void BubbleOneCycle(int* pData, int nLength)
+{
+    for (int i = 1; i < nLength ; i++)
+    {
+        BubbleLogic(pData[i - 1], pData[i]);
+    }
+}
+
+void BubbleSort(int* pData, int nLength)
+{
+    while (nLength > 1)
+    {
+        BubbleOneCycle(pData, nLength);
+        nLength--;
+    }
+}
+
+void PrintData(const int* pData, int nLength)
+{
+    for (int i = 0; i < nLength; i++)
+    {
+        printf("%d ", pData[i]);
+    }
+    printf("\n");
+}
