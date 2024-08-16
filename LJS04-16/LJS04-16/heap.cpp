364fd0c8e9c62b69557a6a3ce71f5b8b0f0904a2
lizhucheng1165 <130527082+lizhucheng1165@users.noreply.github.com>
2024-08-16 13:51:14 +0900

    Add files via upload
    힙 삭제

--- LJS04-16/LJS04-16/heap.cpp
+++ LJS04-16/LJS04-16/heap.cpp
@@ -1,5 +1,35 @@
 #include "heap.h"
 
+void C_HEAP::SwapData(int& nDst, int& nSrc)
+{
+	int nTmp = nDst;
+	nDst = nSrc;
+	nSrc = nTmp;
+}
+
+bool C_HEAP::HasTwoChildNodes(int& nCurrentIndex)
+{
+	int nLeftChildIndex = nCurrentIndex * 2;
+	int nRightChildIndex = nCurrentIndex * 2 + 1;
+
+	if (nLeftChildIndex <= m_nLastIndex && nRightChildIndex <= m_nLastIndex)
+	{
+		return true;
+	}
+
+	return false;
+}
+
+bool C_HEAP::IsLeftChildSmaller(int& nLeftChildValue, int& nRightChildValue)
+{
+	if (nLeftChildValue < nRightChildValue)
+	{
+		return true;
+	}
+
+	return false;
+}
+
 void C_HEAP::Init(int nSize)
 {
 	m_nSize = nSize + 1;
@@ -15,11 +45,17 @@ bool C_HEAP::Add(int nData)
 
 	m_nLastIndex++;
 	m_pData[m_nLastIndex] = nData;
-	
+
 	//반복문을 돌려서
 	//위의 노드 값이 현재 노드보다 크면 바꾼다
 	int nCurrent = m_nLastIndex;
 	int nUp = nCurrent / 2;
+	while (nUp > 0 && m_pData[nUp] > m_pData[nCurrent])
+	{
+		SwapData(m_pData[nUp], m_pData[nCurrent]);
+		nCurrent = nUp;
+		nUp = nCurrent / 2;
+	}
 
 
 	//구현 후 위의 노드가 더 작으면 스탑하는 기능 추가
@@ -34,9 +70,58 @@ void C_HEAP::Release()
 	m_pData = nullptr;
 }
 
+bool C_HEAP::Empty()
+{
+	if (m_nLastIndex = 0)
+	{
+		return true;
+	}
+
+	return false;
+}
+
+int C_HEAP::GetTop()
+{
+	return m_pData[1];
+}
+
+void C_HEAP::Pop()
+{
+	if (m_nLastIndex == 0)
+	{
+		return;
+	}
+
+	m_pData[1] = m_pData[m_nLastIndex];
+	m_nLastIndex--;
+
+	int nCurrentIndex = 1;
+	int nLeftChildIndex = nCurrentIndex * 2;
+	int nRightChildIndex = nCurrentIndex * 2 + 1;
+
+	while (nCurrentIndex != m_nLastIndex && HasTwoChildNodes(nCurrentIndex))
+	{
+		if (IsLeftChildSmaller(m_pData[nLeftChildIndex], m_pData[nRightChildIndex]))
+		{
+			SwapData(m_pData[nCurrentIndex], m_pData[nLeftChildIndex]);
+			nCurrentIndex = nLeftChildIndex;
+		}
+		else
+		{
+			SwapData(m_pData[nCurrentIndex], m_pData[nRightChildIndex]);
+			nCurrentIndex = nRightChildIndex;
+		}
+		nLeftChildIndex = nCurrentIndex * 2;
+		nRightChildIndex = nCurrentIndex * 2 + 1;
+	}
+
+	
+
+}
+
 void C_HEAP::Print()
 {
-	for (int i = 0; i < m_nSize; i++)
+	for (int i = 1; i <= m_nLastIndex; i++)
 	{
 		printf("%d ", m_pData[i]);
 	}
--- LJS04-16/LJS04-16/heap.h
+++ LJS04-16/LJS04-16/heap.h
@@ -8,15 +8,24 @@ private:
 	int m_nSize;
 	int m_nLastIndex;
 
+private:
+	void SwapData(int& nDst, int& nSrc);
+	bool HasTwoChildNodes(int& nCurrentIndex);
+	bool IsLeftChildSmaller(int& nLeftChildValue, int& nRightChildValue);
+
 public:
 	C_HEAP() = default;
 	C_HEAP(const C_HEAP&) = delete;
 	C_HEAP& operator = (const C_HEAP) = delete;
 
+
 	void Init(int nSize);
 	bool Add(int nData);
 	void Release();
-	
+	bool Empty();
+	int GetTop();
+	void Pop();
+
 	void Print();
 
 };
