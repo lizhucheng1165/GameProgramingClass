72a4d5c274b55c1cf3a38c9c25580c0fc4cfe159
lizhucheng1165 <130527082+lizhucheng1165@users.noreply.github.com>
2024-08-20 13:48:27 +0900

    Add files via upload
    재귀함수

---
+++ LJS04-19/LJS04-19/LJS04-19.cpp
@@ -0,0 +1,36 @@
+﻿// LJS04-19.cpp : 이 파일에는 'main' 함수가 포함됩니다. 거기서 프로그램 실행이 시작되고 종료됩니다.
+//
+
+#include <iostream>
+
+void func(int nData)
+{
+	//printf("테스트\n");
+	if (nData < 0)
+	{
+		return;
+	}
+	//func(nData - 1);
+	printf("%d\n", nData);
+	//func(nData - 1);
+	func(nData - 1);
+}
+
+int main()
+{
+	//int nData{};
+	//int* p{};
+
+
+	//nData = 100;
+
+	//p = &nData;
+
+	//*p = 200;
+
+	//printf("%d\n", nData);
+
+	func(3);
+}
+
+
---
+++ LJS04-20/LJS04-20/LJS04-20.cpp
@@ -0,0 +1,26 @@
+﻿// LJS04-20.cpp : 이 파일에는 'main' 함수가 포함됩니다. 거기서 프로그램 실행이 시작되고 종료됩니다.
+//
+
+#include <iostream>
+
+void Add(int nData, int* pResult);
+
+int main()
+{
+	int nData{};
+	Add(5, &nData);
+	printf("%d\n", nData);
+
+}
+
+void Add(int nData, int* pResult)
+{
+	if (nData <= 0)
+	{
+		return;
+	}
+	int nResult{};
+
+	Add(nData - 1, &nResult);
+	*pResult = nResult + nData;
+}
