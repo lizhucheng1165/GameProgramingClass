b28b59d9b2e147e6ad355359db14cf27e0307679
lizhucheng1165 <130527082+lizhucheng1165@users.noreply.github.com>
2024-08-21 13:51:03 +0900

    Add files via upload
    유클리드 호제법
    피보나치

---
+++ LJS04-21/LJS04-21/LJS04-21.cpp
@@ -0,0 +1,50 @@
+﻿// LJS04-21.cpp : 이 파일에는 'main' 함수가 포함됩니다. 거기서 프로그램 실행이 시작되고 종료됩니다.
+//
+
+#include <iostream>
+
+void GCD(int nDst, int nSrc, int& nResult);
+void GetFibonacciValue(int nIndex, int& nResult);
+int main()
+{
+	int nResult{};
+
+	//GCD(16, 30, nResult);
+	GetFibonacciValue(-10, nResult);
+
+	printf("%d", nResult);
+}
+
+void GCD(int nDst, int nSrc, int& nResult)
+{
+	if (nSrc == 0)
+	{
+		nResult = nDst;
+		return;
+	}
+
+	int nRemainder = nDst % nSrc;
+
+	GCD(nSrc, nRemainder, nResult);
+
+
+}
+
+void GetFibonacciValue(int nIndex, int& nResult)
+{
+	
+	if (nIndex <= 1)
+	{
+		nResult = 1;
+		return;
+	}
+
+	int nResult1{};
+	int nResult2{};
+	
+	GetFibonacciValue(nIndex - 1, nResult1);
+	GetFibonacciValue(nIndex - 2, nResult2);
+	
+	nResult = nResult1 + nResult2;
+	
+}
