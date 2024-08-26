31b5de057e8d70cce6fb7d384639b376b67340de
lizhucheng1165 <130527082+lizhucheng1165@users.noreply.github.com>
2024-08-26 13:49:37 +0900

    Add files via upload
    병합정렬

---
+++ LJS04-23/LJS04-23/LJS04-23.cpp
@@ -0,0 +1,63 @@
+﻿#include <iostream>
+
+void swapData(int& nDst, int& nSrc);
+void printData(const int* pData, int nLength);
+void quickSort(int* pData, int nLength);
+
+int main()
+{
+	int arData[9]{ 5,3,8,7,2,9,1,4,6 };
+
+	int* pData = arData;
+	int nLength = 9;
+
+	printData(pData, 9);
+	quickSort(pData, 9);
+	printData(pData, 9);
+}
+
+void swapData(int& nDst, int& nSrc)
+{
+	int nTmp = nDst;
+	nDst = nSrc;
+	nSrc = nTmp;
+}
+
+void printData(const int* pData, int nLength)
+{
+	for (int i = 0; i < nLength; i++)
+	{
+		printf("%d ", pData[i]);
+	}
+	printf("\n");
+}
+
+void quickSort(int* pData, int nLength)
+{
+	if (nLength <= 1)
+	{
+		return;
+	}
+
+	int nPivot = 0;
+	int nLeft = 1;
+	int nRight = nLength - 1;
+
+	while (nLeft <= nRight)
+	{
+		while (nLeft < nLength && pData[nLeft] < pData[nPivot])
+			nLeft++;
+
+		while (pData[nRight] > pData[nPivot])
+			nRight--;
+
+		//if (nRight - nLeft == -1)
+		if (nLeft < nRight)
+			swapData(pData[nLeft], pData[nRight]);
+		else
+			swapData(pData[nPivot], pData[nRight]);
+	}
+
+	quickSort(pData, nRight); //왼쪽부분 정렬
+	quickSort(pData + nLeft, nLength - nLeft); //오른쪽부분 정렬
+}
---
+++ LJS04-24/LJS04-24/LJS04-24.cpp
@@ -0,0 +1,80 @@
+﻿// LJS04-24.cpp : 이 파일에는 'main' 함수가 포함됩니다. 거기서 프로그램 실행이 시작되고 종료됩니다.
+//
+
+#include <iostream>
+void PrintData(const int* pData, int nLength);
+
+int main()
+{
+	int arData[9]{2,4,5,7          ,1,3,6,8,9 };
+	int arTmp[9]{};
+
+	int* pData = arData;
+	int nLength = 9;
+	int* pTmp = arTmp;
+
+	int* pLeft = pData;
+	int nLeftLength = nLength / 2;
+	int* pRight = pData + nLeftLength;
+	int nRightLength = nLength - nLeftLength;
+
+	int nLeftIndex{};
+	int nRightIndex{};
+	int nTmpIndex{};
+
+	PrintData(pLeft, nLeftLength);
+	PrintData(pRight, nRightLength);
+
+
+	while (nLeftIndex < nLeftLength && nRightIndex < nRightLength)
+	{
+		int* pMin = pLeft;
+		int* pIndex = &nLeftIndex;
+		if (pLeft[nLeftIndex] < pRight[nRightIndex])
+		{
+			pMin = pRight;
+			pIndex = &nRightIndex;
+		}
+		pTmp[nTmpIndex] = pMin[*pIndex];
+		nTmpIndex++;
+		(*pIndex)++;
+
+		//if (pLeft[nLeftIndex] < pRight[nRightIndex])
+		//{
+		//	pTmp[nTmpIndex] = pLeft[nLeftIndex];
+		//	nLeftIndex++;
+		//}
+		//else
+		//{
+		//	pTmp[nTmpIndex] = pRight[nRightIndex];
+		//	nRightIndex++;
+		//}
+		//nTmpIndex++;
+	}
+
+	while (nLeftIndex < nLeftLength)
+	{
+		pTmp[nTmpIndex] = pLeft[nLeftIndex];
+		nLeftIndex++;
+		nTmpIndex++;
+	}
+	while (nRightIndex < nRightLength)
+	{
+		pTmp[nTmpIndex] = pRight[nRightIndex];
+		nRightIndex++;
+		nTmpIndex++;
+	}
+
+
+
+	PrintData(pTmp, nLength);
+}
+
+void PrintData(const int* pData, int nLength)
+{
+	for (int i = 0; i < nLength; i++)
+	{
+		printf("%d ", pData[i]);
+	}
+	printf("\n");
+}
