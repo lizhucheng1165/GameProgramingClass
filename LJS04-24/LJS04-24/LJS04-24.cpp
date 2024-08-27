251567223df9cb79f2773346ed42f849149befb6
lizhucheng1165 <130527082+lizhucheng1165@users.noreply.github.com>
2024-08-27 13:46:21 +0900

    Add files via upload
    하노이의 탑, 병합정렬

--- LJS04-24/LJS04-24/LJS04-24.cpp
+++ LJS04-24/LJS04-24/LJS04-24.cpp
@@ -3,68 +3,19 @@
 
 #include <iostream>
 void PrintData(const int* pData, int nLength);
+void MergeSort(int* pData, int nLength, int* pTmp);
 
 int main()
 {
-	int arData[9]{2,4,5,7          ,1,3,6,8,9 };
+	int arData[9]{1,5,6,3,2,4,7,8,9 };
 	int arTmp[9]{};
 
 	int* pData = arData;
 	int nLength = 9;
 	int* pTmp = arTmp;
 
-	int* pLeft = pData;
-	int nLeftLength = nLength / 2;
-	int* pRight = pData + nLeftLength;
-	int nRightLength = nLength - nLeftLength;
-
-	int nLeftIndex{};
-	int nRightIndex{};
-	int nTmpIndex{};
-
-	PrintData(pLeft, nLeftLength);
-	PrintData(pRight, nRightLength);
-
-
-	while (nLeftIndex < nLeftLength && nRightIndex < nRightLength)
-	{
-		int* pMin = pLeft;
-		int* pIndex = &nLeftIndex;
-		if (pLeft[nLeftIndex] < pRight[nRightIndex])
-		{
-			pMin = pRight;
-			pIndex = &nRightIndex;
-		}
-		pTmp[nTmpIndex] = pMin[*pIndex];
-		nTmpIndex++;
-		(*pIndex)++;
-
-		//if (pLeft[nLeftIndex] < pRight[nRightIndex])
-		//{
-		//	pTmp[nTmpIndex] = pLeft[nLeftIndex];
-		//	nLeftIndex++;
-		//}
-		//else
-		//{
-		//	pTmp[nTmpIndex] = pRight[nRightIndex];
-		//	nRightIndex++;
-		//}
-		//nTmpIndex++;
-	}
-
-	while (nLeftIndex < nLeftLength)
-	{
-		pTmp[nTmpIndex] = pLeft[nLeftIndex];
-		nLeftIndex++;
-		nTmpIndex++;
-	}
-	while (nRightIndex < nRightLength)
-	{
-		pTmp[nTmpIndex] = pRight[nRightIndex];
-		nRightIndex++;
-		nTmpIndex++;
-	}
-
+	PrintData(arData, nLength);
+	MergeSort(pData, nLength, pTmp);
 
 
 	PrintData(pTmp, nLength);
@@ -78,3 +29,50 @@ void PrintData(const int* pData, int nLength)
 	}
 	printf("\n");
 }
+
+// Must pTmpLength >= nLength
+void MergeSort(int* pData, int nLength, int* pTmp)
+{
+	if (nLength <= 1)
+	{
+		return;
+	}
+
+	int* pLeft = pData;
+	int nLeftLength = nLength / 2;
+	int* pRight = pData + nLeftLength;
+	int nRightLength = nLength - nLeftLength;
+
+	int nLeftIndex{};
+	int nRightIndex{};
+	int nTmpIndex{};
+
+	//PrintData(pLeft, nLeftLength);
+	//PrintData(pRight, nRightLength);
+	MergeSort(pLeft, nLeftLength, pTmp);
+	MergeSort(pRight, nRightLength, pTmp);
+
+
+	for (int i = 0; i < nLength; i++)
+	{
+		int* pMin = pLeft;
+		int* pMinIndex = &nLeftIndex;
+
+		if (nLeftIndex >= nLeftLength || (nRightIndex < nRightLength && pLeft[nLeftIndex] > pRight[nRightIndex]))
+		{
+			pMin = pRight;
+			pMinIndex = &nRightIndex;
+		}
+
+		pTmp[i] = pMin[*pMinIndex];
+		(*pMinIndex)++;
+	}
+
+
+	for (int i = 0; i < nLength; i++)
+	{
+		pData[i] = pTmp[i];
+	}
+}
+
+
---
+++ LJS04-25/LJS04-25/LJS04-25.cpp
@@ -0,0 +1,25 @@
+﻿// LJS04-25.cpp : 이 파일에는 'main' 함수가 포함됩니다. 거기서 프로그램 실행이 시작되고 종료됩니다.
+//
+
+#include <iostream>
+
+void Hanoi(int nNum, char S, char T, char E);
+
+int main()
+{
+	Hanoi(1, 'A', 'B', 'C');
+}
+
+void Hanoi(int nNum, char S, char T, char E)
+{
+	if (nNum < 1)
+	{
+		//printf("%d %c -> %c\n", nNum, S, E);
+		return;
+	}
+
+	Hanoi(nNum - 1, S, E, T);
+	printf("%d %c -> %c\n", nNum, S, E);
+	//printf("%d -> %c\n", nNum, E);
+	Hanoi(nNum - 1, T, S, E);
+}
