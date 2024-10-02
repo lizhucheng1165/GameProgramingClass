38c0f6774c6b909f72f5b329d205a686b50533c7
lizhucheng1165 <130527082+lizhucheng1165@users.noreply.github.com>
2024-10-02 13:51:45 +0900

    Add files via upload
    vector reserve resize

---
+++ LJS05-12/LJS05-12/LJS05-12.cpp
@@ -0,0 +1,42 @@
+﻿// LJS05-12.cpp : 이 파일에는 'main' 함수가 포함됩니다. 거기서 프로그램 실행이 시작되고 종료됩니다.
+//
+
+#include <iostream>
+#include <vector>
+
+int main()
+{
+	//std::vector<int> vData(4);
+
+	//vData.push_back(9);
+
+	//for (int i = 0; i < 5; i++)
+	//{
+	//	printf("%d ", vData[i]);
+	//}
+
+	//std::vector<int> vData{};
+
+	//vData.reserve(10);
+	//for (int i = 0; i < 10; i++)
+	//{
+	//	vData.push_back(i);
+	//	printf("전체크기 : %d, 사용크기 : %d\n ", vData.capacity(), vData.size());
+	//}
+
+	//vData.pop_back();
+	//printf("전체크기 : %d, 사용크기 : %d\n ", vData.capacity(), vData.size());
+
+	std::vector<int> vData{};
+
+	vData.reserve(10);
+	vData.resize(10);
+
+	vData[5] = 99;
+	for (int i = 0; i < vData.size(); i++)
+	{
+		printf("%d\n", vData[i]);
+	}
+
+
+}
