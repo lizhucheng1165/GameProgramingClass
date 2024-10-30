68637dd503f54284d1e756398fb3b48e8bcb87db
lizhucheng1165 <130527082+lizhucheng1165@users.noreply.github.com>
2024-10-30 13:41:31 +0900

    Add files via upload
    api 행렬곱 위치, 크기 조정

---
+++ LJS05-30/LJS05-30/LJS05-30.cpp
@@ -0,0 +1,39 @@
+﻿// LJS05-30.cpp : 이 파일에는 'main' 함수가 포함됩니다. 거기서 프로그램 실행이 시작되고 종료됩니다.
+//
+
+#include <iostream>
+#include <D3DX11.h>
+#include <D3D11.h>
+#include <xnamath.h>
+
+void PrintVector(FXMVECTOR v);
+
+int main()
+{
+	XMMATRIX matScale{};
+	XMMATRIX matRotation{};
+	XMMATRIX matLocation{};
+	XMFLOAT3 f3Pos{};
+	XMVECTOR vPos{};
+	XMVECTOR vResult{};
+	
+
+	f3Pos = XMFLOAT3(3.0f, 1.0f, 1.0f);
+	matScale = XMMatrixIdentity();
+	matRotation = XMMatrixIdentity();
+	matLocation = XMMatrixIdentity();
+
+	vPos = XMLoadFloat3(&f3Pos);
+	matScale = XMMatrixScaling(2.0f, 2.0f, 2.0f);
+	vResult = XMVector3Transform(vPos, matScale);
+	PrintVector(vResult);
+
+	matLocation = XMMatrixTranslation(5.0f, 0.0f, 0.0f);
+	vResult = XMVector3Transform(vPos, matLocation);
+	PrintVector(vResult);
+}
+
+void PrintVector(FXMVECTOR v)
+{
+	printf("%f, %f, %f\n", v.m128_f32[0], v.m128_f32[1], v.m128_f32[2]);
+}
